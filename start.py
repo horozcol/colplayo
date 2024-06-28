08783104de5ed3cbe8d52a7aba91731028385290
hugo <horozcol@gmail.com>
2024-06-28 12:05:39 -0400

    import os os.environ["XDG_SESSION_TYPE"] = "xcb" trying to solve pycharm freezing

--- start.py
+++ start.py
@@ -1,8 +1,10 @@
 from ultralytics import YOLO
 import cv2
-from sort import *
 from util import get_car, read_license_plate
+from colorama import Fore, Back, Style
+import os
 
+os.environ["XDG_SESSION_TYPE"] = "xcb"
 #dictionary
 results = {}
 # load models
@@ -11,12 +13,16 @@ license_plate_detector = YOLO('license_plate_detector.pt')
 # class we are looking for
 vehicles = [2, 3, 4, 5]
 # load source
-#cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture('./centro2.mp4')
+cam = 0
+
+#cap = cv2.VideoCapture('./centro2.mp4')
+cap = cv2.VideoCapture(0)
+
 
 # read frames
 ret = True
 while ret:
+
     ret, frame = cap.read()
 
     if ret:
@@ -24,9 +30,6 @@ while ret:
         detections = coco_model(frame)[0]
     # if the class id we looking for is in the actual detection, append to the variable called detections_
         detections_ = []
-
-
-
 
 
         for detection in detections.boxes.data.tolist():
@@ -49,7 +52,7 @@ while ret:
                 _, license_plate_crop_thresh = cv2.threshold(license_plate_crop_gray, 80, 255, cv2.THRESH_BINARY_INV)
                 cv2.imshow('original', license_plate_crop)
                 cv2.imshow('threshold', license_plate_crop_thresh)
-                cv2.waitKey(10)
+
 
                 # read license plate
                 license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_thresh)
@@ -59,10 +62,15 @@ while ret:
                                                          'text': license_plate_text,
                                                          'bbox_score': score,
                                                          'text_score': license_plate_text_score}}
-                    print( f'\n \n Placa detectada: {license_plate_text}\n score: {100*score: .2f} % \n text score: {100*license_plate_text_score: .2f} %' )
-                    #print(results[car_id])
-
-                #write the results
+                    print(Back.YELLOW + f'\n \n Placa detectada: {license_plate_text}\n score: {100*score: .2f} % \n text score: {100*license_plate_text_score: .2f} %' )
+                    print(Back.RESET)
+    # cv2.waitKey(1000)
+    #
+    # if cam == 0:
+    #     cam = 2
+    #
+    # else:
+    #     cam =0
 
 
 
